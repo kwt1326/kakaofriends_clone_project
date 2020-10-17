4f53ad35f33edfd49794feb69319f2585a1cd87e
kwt1326 <u1326@hotmail.com>
2020-10-17 23:19:17 +0900

    first commit

---
+++ front_end/src/Components/Header/index.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import * as Router from 'react-router';
+
+class Header extends Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Header</h1>
+    )
+  }
+}
+
+export default Header;
---
+++ front_end/src/Pages/router.tsx
@@ -0,0 +1,36 @@
+import React, { Component, Fragment } from 'react';
+import { Route, Switch } from 'react-router';
+import axios from 'axios';
+
+import Header from '../Components/Header';
+import Home from './Home'
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+class Router extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(routeProps: any) => <Home {...routeProps} />} />
+          {/* <Route
+            exact
+            path="/login"
+            render={(routeProps: any) => <Login {...routeProps} />} />
+          <Route
+            exact
+            path="/register"
+            render={(routeProps: any) => <Register {...routeProps} />} /> */}
+        </Switch>
+      </Fragment>
+    )
+  }
+}
+
+export default Router;
