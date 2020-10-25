f548cbaf20b30d93109422816242d3ddc6800a6b
kwt1326 <u1326@hotmail.com>
2020-10-26 00:36:33 +0900

    callapi example

--- front_end/src/Pages/Home/index.tsx
+++ front_end/src/Pages/Home/index.tsx
@@ -2,23 +2,24 @@ import React, { useEffect } from 'react';
 import { axiosApiCall, callApiProps } from '../../utils/callApi'
 
 const Home = (props: any) => {
-  const applyProduct = async () => {
-    const data: callApiProps = {
-      method: 'POST',
-      url: 'api/product',
-      body: {
-        product_name: 'test_from_front',
-        category: 'others',
-        price: '20000',
-      }
-    }
-    const response = await axiosApiCall(data);
-    console.log(response);
-  }
+  // @callapi 예시
+  // const applyProduct = async () => {
+  //   const data: callApiProps = {
+  //     method: 'POST',
+  //     url: 'api/product',
+  //     body: {
+  //       product_name: 'test_from_front',
+  //       category: 'others',
+  //       price: '20000',
+  //     }
+  //   }
+  //   const response = await axiosApiCall(data);
+  //   console.log(response);
+  // }
 
-  useEffect(() => {
-    applyProduct()
-  })
+  // useEffect(() => {
+  //   applyProduct()
+  // })
 
   return (
     <h1>hello world!</h1>
