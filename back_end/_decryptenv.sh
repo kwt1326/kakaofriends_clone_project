e692ae5eaa33730b839aac255d4d6c612b3a43a1
kwt1326 <u1326@hotmail.com>
2020-11-20 01:45:00 +0900

    fix sh

--- back_end/_decryptenv.sh
+++ back_end/_decryptenv.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mkdir $HOME/back_end/env
+ls -al
+mkdir ./env
 gpg --quiet --batch --yes --decrypt --passphrase="$ENV_DECRYPE_PW" \
---output $HOME/back_end/env/env.json ./back_end/env.json.gpg
+--output ./env/env.json env.json.gpg
